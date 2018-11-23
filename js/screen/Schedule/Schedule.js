3acbe866f92889c263e6eb4ffde2687a7b8ed7f0
elim322 <einer.lim322@gmail.com>
2018-11-23 14:49:41 -0800

    print data out on schedule page with moments.js for time

--- js/screen/Schedule/Schedule.js
+++ js/screen/Schedule/Schedule.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import {
   View,
@@ -8,22 +8,34 @@ import {
   StatusBar,
   Platform
 } from "react-native";
+import moment from "moment";
+import styles from "./styles";
 
 const Schedule = ({ sessions }) => {
   console.log(sessions);
   return (
     <View style={styles.container}>
       <StatusBar barStyle="light-content" />
-
       <SectionList
-        renderItem={({ item, index }) => <Text item={item} index={index} />}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.title}</Text>
+            <Text>{item.location}</Text>
+            <Ionicons
+              name="ios-heart"
+              size={"horizontal" ? 20 : 25}
+              color="red"
+            />
+          </View>
+        )}
         renderSectionHeader={({ section }) => (
-          <Text style={styles.header}>{sessions.title}</Text>
+          <Text style={styles.header}>
+            {moment(section.title).format("LT")}
+          </Text>
         )}
         sections={sessions}
-        keyExtractor={(item, index) => item.title}
+        keyExtractor={(item, index) => item + index}
       />
-      <Ionicons name="ios-heart" size={"horizontal" ? 20 : 25} color="red" />
     </View>
   );
 };
