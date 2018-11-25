99ebba714fe297a7915a352619dbc93cabcbc2ca
elim322 <einer.lim322@gmail.com>
2018-11-25 01:53:43 -0800

    edit styles and layout

--- js/screen/Faves/Faves.js
+++ js/screen/Faves/Faves.js
@@ -1,24 +1,34 @@
 import React from "react";
-import { View, Text, StyleSheet, SectionList } from "react-native";
-import { FavesContext } from "../../context/FavesContext/FavesProvider";
-import styles from "./styles";
+import { View, Text, SectionList } from "react-native";
+import styles from "../Schedule/styles";
 
 const Faves = ({ faves }) => {
   return (
     <View style={styles.container}>
+      <StatusBar barStyle="light-content" />
       <SectionList
         renderItem={({ item }) => (
-          <View style={styles.separator}>
-            <View style={styles.content}>
-              <Text style={styles.title}>{item.title}</Text>
-              <Text style={styles.location}>
-                {item.location}
-                {props.faveIds.find(fave => fave === item.id) && (
-                  <Ionicon name={"ios-heart"} color={globalStyles.red.color} />
+          <TouchableHighlight
+            onPress={() => {
+              navigation.navigate("Session", { id: item.id });
+            }}
+            underlayColor="#e6e6e6"
+            activeOpacity={0.7}
+          >
+            <View style={styles.separator}>
+              <View style={styles.content}>
+                <Text style={styles.title}>{item.title}</Text>
+                <Text style={styles.location}>{item.location}</Text>
+                {faveIds.find(item => item === item.id) && (
+                  <Ionicons
+                    name="ios-heart"
+                    size={"horizontal" ? 20 : 25}
+                    color="red"
+                  />
                 )}
-              </Text>
+              </View>
             </View>
-          </View>
+          </TouchableHighlight>
         )}
         renderSectionHeader={({ section }) => (
           <Text style={styles.header}>
