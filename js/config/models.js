8e89b4337311298dd33dcbe0fc7ce68e68dc9f5e
elim322 <einer.lim322@gmail.com>
2018-11-24 23:58:09 -0800

    move methods to provider

--- js/config/models.js
+++ js/config/models.js
@@ -11,20 +11,4 @@ const Faves = {
 
 const realm = new Realm({ schema: [Faves] });
 
-export const createFaves = id => {
-  realm.write(() => {
-    realm.create("Faves", { id: id, faved_on: new Date() });
-  });
-};
-export const removeFave = id => {
-  let fave = realm.objects("Faves").filtered(`id ==$0`, id);
-  realm.write(() => {
-    realm.delete(fave);
-  });
-};
-
-export const queryAllFaves = () => {
-  return realm.objects("Faves");
-};
-
 export default realm;
