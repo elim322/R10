83fcec97579b915efd1e10c017c38368f56d58bd
elim322 <einer.lim322@gmail.com>
2018-11-26 02:00:21 -0800

    style session page

--- js/screen/Session/Session.js
+++ js/screen/Session/Session.js
@@ -7,10 +7,12 @@ import {
   Image,
   TouchableHighlight,
   Modal,
-  TouchableOpacity
+  TouchableOpacity,
+  Platform
 } from "react-native";
 import moment from "moment";
 import styles from "./styles";
+import LinearGradient from "react-native-linear-gradient";
 
 class Session extends Component {
   constructor(props) {
@@ -25,21 +27,26 @@ class Session extends Component {
   }
 
   render() {
-    console.log(this.props.methods);
+    const iconName = Platform.select({
+      ios: "ios-heart",
+      android: "md-heart"
+    });
+
     return (
-      <View>
-        <Text>
-          {this.props.data.location}
-          <Ionicons
-            name="ios-heart"
-            size={"horizontal" ? 20 : 25}
-            color="red"
-          />
+      <View style={styles.container}>
+        <Text style={styles.location}>{this.props.data.location}</Text>
+        <Ionicons
+          style={styles.heart}
+          name={iconName}
+          size={"horizontal" ? 20 : 25}
+          color="red"
+        />
+        <Text style={styles.title}>{this.props.data.title}</Text>
+        <Text style={styles.startTime}>
+          {moment(this.props.data.startTime).format("LT")}
         </Text>
-        <Text>{this.props.data.title}</Text>
-        <Text>{moment(this.props.data.startTime).format("LT")}</Text>
-        <Text>{this.props.data.description}</Text>
-        <Text>Presented By:</Text>
+        <Text style={styles.description}>{this.props.data.description}</Text>
+        <Text style={styles.presented}>Presented By:</Text>
         <TouchableHighlight
           onPress={() => {
             this.setModalVisible(!this.state.modalVisible);
@@ -47,14 +54,17 @@ class Session extends Component {
           underlayColor="#e6e6e6"
           activeOpacity={0.7}
         >
-          <Text>
+          <React.Fragment>
             <Image
-              style={{ height: 50, width: 50 }}
+              style={styles.speaker}
               source={{ uri: this.props.data.speaker.image }}
             />
-            {this.props.data.speaker.name}
-          </Text>
+            <Text style={styles.speakerName}>
+              {this.props.data.speaker.name}
+            </Text>
+          </React.Fragment>
         </TouchableHighlight>
+
         <View>
           {this.props.faveIds.includes(this.props.data.id) ? (
             <TouchableOpacity
@@ -62,7 +72,14 @@ class Session extends Component {
                 this.props.methods.deleteFave(this.props.data.id);
               }}
             >
-              <Text style={styles.button}>Remove From Faves</Text>
+              <LinearGradient
+                colors={["#9963ea", "#8797D6"]}
+                start={{ x: 0.0, y: 0.0 }}
+                end={{ x: 1.0, y: 1.0 }}
+                style={styles.button}
+              >
+                <Text style={styles.buttonTitle}>Remove From Faves</Text>
+              </LinearGradient>
             </TouchableOpacity>
           ) : (
             <TouchableOpacity
@@ -70,7 +87,14 @@ class Session extends Component {
                 this.props.methods.createFave(this.props.data.id);
               }}
             >
-              <Text style={styles.button}>Add To Faves</Text>
+              <LinearGradient
+                colors={["#9963ea", "#8797D6"]}
+                start={{ x: 0.0, y: 0.0 }}
+                end={{ x: 1.0, y: 1.0 }}
+                style={styles.button}
+              >
+                <Text style={styles.buttonTitle}>Add To Faves</Text>
+              </LinearGradient>
             </TouchableOpacity>
           )}
         </View>
--- js/screen/Session/styles.js
+++ js/screen/Session/styles.js
@@ -1,2 +1,48 @@
 import { StyleSheet } from "react-native";
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    padding: 10
+  },
+  location: {
+    color: "#999"
+  },
+  heart: {
+    position: "absolute",
+    right: 8,
+    top: 10
+  },
+  title: {
+    fontSize: 27,
+    paddingTop: 10
+  },
+  startTime: {
+    color: "#cf392a",
+    paddingTop: 10
+  },
+  description: {
+    fontSize: 16,
+    paddingTop: 10
+  },
+  speaker: {
+    height: 50,
+    width: 50,
+    borderRadius: 25
+  },
+  presented: {
+    paddingTop: 10
+  },
+  button: {
+    marginTop: 10,
+    padding: 10,
+    borderRadius: 25,
+    alignItems: "center"
+  },
+  buttonTitle: {
+    textAlign: "center",
+    color: "white",
+    fontSize: 14.5,
+    lineHeight: 15 * 1.5
+  }
+});
+
+export default styles;
